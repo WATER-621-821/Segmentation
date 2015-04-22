8cbe683c192bbdd7ce707ecd569f03ce21bf7713
unknown <sashi_thapaliya@hotmail.com>
2015-04-22 15:06:25 -0700

    Comments added to GaussianPyramid.h file

--- src/GaussianPyramid.h
+++ src/GaussianPyramid.h
@@ -22,7 +22,19 @@ class GaussianPyramid
 
 	public:
 
+		/**
+ 			@parameter img is the name of the input image
+        		if the image is not in the current directory then full path along with the name must be given
+ 			@parameter otImg is the name of the output image
+				output image will be saved in the current directory where the executable resides.
+		**/
 		GaussianPyramid(string inImg, string otImg);
+
+
+		/**
+			DownSamples the input image
+			Creates the new down sampled image with the set output Image name in the current directory of the executable.
+		**/	
 		void execute();
 
 		
